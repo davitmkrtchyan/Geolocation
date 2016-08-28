54fcaa62627fd08b665a5abc8bb50caef278ecdb
David Mkrtchyan <davo991@mail.ru>
2016-08-29 01:31:25 +0400

    Ranndom locations

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -10,13 +10,30 @@ function getLocation() {
 function showPosition(position) {
     lat = position.coords.latitude;
     lon = position.coords.longitude;
-    latlon = new google.maps.LatLng(lat, lon)
+    latlon = new google.maps.LatLng(lat, lon);
+    function ranFunc(){
+            ran = Math.floor((Math.random() * 1000) + 1);
+            ran = ran / 1000000;
+    }
+    ranFunc();
+    var myLatlng1 = new google.maps.LatLng(lat+(ran*4), lon+(ran*3));
+    ranFunc();
+    var myLatlng2 = new google.maps.LatLng(lat-ran, lon-ran);
+    ranFunc();
+    var myLatlng3 = new google.maps.LatLng(lat+(ran*2), lon+ran);
+    ranFunc();
+    var myLatlng4 = new google.maps.LatLng(lat+ran, lon+ran);
+    ranFunc();
+    var myLatlng5 = new google.maps.LatLng(lat+ran, lon+(ran*2));
+    ranFunc();
+    var myLatlng6 = new google.maps.LatLng(lat+(ran*3), lon+ran);
+
     mapholder = document.getElementById('mapholder')
     mapholder.style.height = '380px';
     mapholder.style.width = '100%';
 
     var myOptions = {
-    center:latlon,zoom:19,
+    center:latlon,zoom:15,
     mapTypeId:google.maps.MapTypeId.ROADMAP,
     mapTypeControl:false,
     navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
@@ -24,6 +41,12 @@ function showPosition(position) {
     
     var map = new google.maps.Map(document.getElementById("mapholder"), myOptions);
     var marker = new google.maps.Marker({position:latlon,map:map,title:"You are here!"});
+    var marker1 = new google.maps.Marker({position:myLatlng1,map:map,title:"You are here!"});
+    var marker2 = new google.maps.Marker({position:myLatlng2,map:map,title:"You are here!"});
+    var marker3 = new google.maps.Marker({position:myLatlng3,map:map,title:"You are here!"});
+    var marker4 = new google.maps.Marker({position:myLatlng4,map:map,title:"You are here!"});
+    var marker5 = new google.maps.Marker({position:myLatlng5,map:map,title:"You are here!"});
+    var marker6 = new google.maps.Marker({position:myLatlng6,map:map,title:"You are here!"});
 }
 
 function showError(error) {
@@ -64,6 +87,5 @@ $("#menu-toggle").click(function(e) {
         $("#wrapper").toggleClass("toggled");
     });
 
-   
 });
 
